4e251f2ae0acd238beb9cede8509ec8a410faeab
gaura <gauraputu@gmail.com>
2022-03-09 22:48:27 +0800

    add option to choose data center location  zoho account can only generate access token on the same data center their account are created

--- zoho-oauth-generator.sh
+++ zoho-oauth-generator.sh
@@ -14,19 +14,25 @@ IN="https://accounts.zoho.in"
 CN="https://accounts.zoho.com.cn"
 JP="https://accounts.zoho.jp"
 myArray=("US" "AU" "EU" "IN" "CN" "JP")
-# printf "US/AU/EU/IN/CN/JP , default US\n"
-# read -p "Choose data center: " dataCenter
+printf "US/AU/EU/IN/CN/JP , default US\n"
+read -p "Choose data center: " dataCenter
 
-# arrayLength=${#myArray[@]}
-# for (( i=0;i<${arrayLength};i++)); do
-#   if [ $dataCenter=${myArray[$i]} ]
-#   then
-#     dataCenter="${${myArray[$i]}}"
-#   else
-#     dataCenter="$US"
-#   fi
-# done
+# echo "dataCenter val: ${dataCenter}"
+if [ ${dataCenter} == 'US' ];then
+  dataCenter="$US/oauth/v2/token"
+elif [ ${dataCenter} == 'AU' ];then
+  dataCenter="$EU/oauth/v2/token"
+elif [ ${dataCenter} == 'EU' ];then
+  dataCenter="$AU/oauth/v2/token"
+elif [ ${dataCenter} == 'IN' ];then
+  dataCenter="$IN/oauth/v2/token"
+elif [ ${dataCenter} == 'CN' ];then
+  dataCenter="$CN/oauth/v2/token"
+elif [ ${dataCenter} == 'JP' ];then
+  dataCenter="$JP/oauth/v2/token"
+else
+  dataCenter="$US/oauth/v2/token"
+fi
 
-dataCenter="$AU/oauth/v2/token"
-echo "$dataCenter"
+# echo "dataCenter val after: $dataCenter"
 curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=authorization_code&code=${grantToken}&client_id=${clientID}&client_secret=${clientSecret}" ${dataCenter} 
